5de3daee0989b935ca84e6f9a313d73dc4476322
Sam Wronski <runewake2@outlook.com>
2016-12-30 23:03:13 -0600

    Add twitter plugin to enable following specific twitter phrases and graphing them.

---
+++ Plugins/DataSphere.Plugins/src/DataSphere.Plugins.Twitter/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using DataSphere.Plugins.RandomGeneratorExample;
+using Newtonsoft.Json;
+using Tweetinvi;
+using Tweetinvi.Models;
+
+namespace DataSphere.Plugins.Twitter
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string filter = String.Empty;
+            while (String.IsNullOrWhiteSpace(filter))
+            {
+                Console.WriteLine("What do you want to display? ");
+                filter = Console.ReadLine();
+            }
+
+            Common.DataSphereClient client = new Common.DataSphereClient("127.0.0.1", 9001);
+            Auth.SetUserCredentials(args[0], args[1], args[2], args[3]);
+
+            var stream = Stream.CreateFilteredStream();
+            stream.AddTrack(filter);
+            stream.MatchingTweetReceived += (sender, message) =>
+            {
+                //Console.WriteLine("A tweet containing " + filter + " has been found; the tweet is '" + message.Tweet + "'");
+                var tweet = JsonConvert.DeserializeObject<TweetViewModel>(message.Json);
+                if (tweet?.place?.boundingBox != null)
+                {
+                    foreach (var box in tweet.place.boundingBox.coordinates)
+                    {
+                        foreach (var coordinate in box)
+                        {
+                            SubmitMessage(client, coordinate);
+                            break;
+                        }
+                    }
+                }
+                //foreach (var location in message.MatchingLocations)
+                //{
+                //    SubmitMessage(client, location.Coordinate1);
+                //    SubmitMessage(client, location.Coordinate2);
+                //}
+            };
+            stream.StartStreamMatchingAllConditions();
+
+            while (true)
+            {
+                Task.Delay(1000);
+                Console.WriteLine("Waiting...");
+            }
+        }
+
+        private static void SubmitMessage(Common.DataSphereClient client, double[] coordinate)
+        {
+            if (coordinate != null)
+            {
+                Console.WriteLine("GRAPHING COORDINATE: " + coordinate);
+                var dataPoint = new DataPointViewModel()
+                {
+                    color = new DataPointColor(),
+                    declination = (float) coordinate[0]/360.0f,
+                    rightAscension = (float) (coordinate[1])/90.0f
+                };
+                var message = JsonConvert.SerializeObject(dataPoint);
+                client.SendMessageToServerTaskAsync(message);
+            }
+        }
+    }
+}
---
+++ Plugins/DataSphere.Plugins/src/DataSphere.Plugins.Twitter/TweetViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace DataSphere.Plugins.Twitter
+{
+    public class TweetViewModel
+    {
+        public TweetPlaceViewModel place;
+    }
+
+    public class TweetPlaceViewModel
+    {
+        [JsonProperty("bounding_box")]
+        public BoundingBoxViewModel boundingBox;
+    }
+
+    public class BoundingBoxViewModel
+    {
+        public IEnumerable<IEnumerable<double[]>> coordinates;
+    }
+}
